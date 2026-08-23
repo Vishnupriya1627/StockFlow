2205811648b04ff857395e742784594dda34c8f0
Vishnupriya <mamillpallivishnpriya@gmail.com>
2026-08-23 17:23:58 +0530

    fixed cors

--- server/index.js
+++ server/index.js
@@ -3,42 +3,43 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const cookieParser = require('cookie-parser');
-const clientIdMiddleware = require('./middleware/clientIdMiddleware');
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+const cookieParser = require("cookie-parser");
+const clientIdMiddleware = require("./middleware/clientIdMiddleware");
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://stock-flow-nu-nine.vercel.app"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(clientIdMiddleware);
 
-const userRoutes = require('./routes/userRoutes');
-app.use('/users',userRoutes);
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
 
-const productRoutes = require('./routes/Productroutes');
-const dashboardRoutes = require('./routes/Dashboardroutes');
-const orderRoutes = require('./routes/Orderroutes');
-app.use('/products', productRoutes);
-app.use('/dashboard', dashboardRoutes);
-app.use('/orders', orderRoutes);
+const productRoutes = require("./routes/Productroutes");
+const dashboardRoutes = require("./routes/Dashboardroutes");
+const orderRoutes = require("./routes/Orderroutes");
+app.use("/products", productRoutes);
+app.use("/dashboard", dashboardRoutes);
+app.use("/orders", orderRoutes);
 
-
-const flashSaleRoutes = require('./routes/flashSaleRoutes');
-app.use('/flashsale', flashSaleRoutes);
-const startFlashSaleScheduler = require('./scheduler/flashSaleScheduler');
+const flashSaleRoutes = require("./routes/flashSaleRoutes");
+app.use("/flashsale", flashSaleRoutes);
+const startFlashSaleScheduler = require("./scheduler/flashSaleScheduler");
 
 const {
-    startReservationExpiryWorker,
-} = require('./scheduler/reservationExpiryWorker');
+  startReservationExpiryWorker,
+} = require("./scheduler/reservationExpiryWorker");
 
-const alertRoutes = require('./routes/alertRoutes');
-app.use('/alerts', alertRoutes);
+const alertRoutes = require("./routes/alertRoutes");
+app.use("/alerts", alertRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -52,17 +53,17 @@ mongoose
     console.log("Err occured", err);
   });
 
-const http = require('http');
+const http = require("http");
 const httpServer = http.createServer(app);
 
-const initSocketServer = require('./socket/socketServer');
+const initSocketServer = require("./socket/socketServer");
 initSocketServer(httpServer);
 
-const { emitQueuePromotion } = require('./socket/socketManager');
-const { subscribeToPromotions } = require('./redis/queuePubSub');
+const { emitQueuePromotion } = require("./socket/socketManager");
+const { subscribeToPromotions } = require("./redis/queuePubSub");
 
 subscribeToPromotions((productId, clientId) => {
-    emitQueuePromotion(productId, clientId);
+  emitQueuePromotion(productId, clientId);
 });
 
 httpServer.listen(3000, () => {
--- server/socket/socketServer.js
+++ server/socket/socketServer.js
@@ -59,6 +59,7 @@ function initSocketServer(httpServer) {
         "http://localhost:5173",
         "http://127.0.0.1:5501",
         "http://localhost:5501",
+        'https://stock-flow-nu-nine.vercel.app',
       ],
       credentials: true,
     },
